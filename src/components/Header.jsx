b4460c139608161755c9443e8ef35a6fa6356729
David Sarif <you@example.comDavidSarif@yandex.ru>
2020-09-11 00:25:15 +0300

    feat:add some stuff

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className="header">
+        <img src="https://git-scm.com/images/logos/downloads/Git-Icon-Black.png" alt="logo" />
+    </header>
+
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return <main className="content">
+        <div>
+            <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" alt="" />
+        </div>
+        <div>
+            avatar + descr
+        </div>
+        <div>
+            my posts
+          <div>
+                new post
+          </div>
+            <div>
+                <div>
+                    post 1
+            </div>
+                <div>
+                    post 2
+            </div>
+            </div>
+        </div>
+    </main>
+}
+
+export default Profile
